ecafc4d974572a56e524556a39880b4d77b268cf
twamix <twamix@gmail.com>
2024-07-13 17:45:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -225,7 +225,7 @@ echo ' |_| \_\__,_|\__, | |____/|_|_| |_|\__, | '
 echo '             |___/                 |___/  '
 
 # ------------vars-----------、
-gitRowUrl="https://raw.githubusercontent.com/RayWangQvQ/sing-box-installer/main"
+gitRowUrl="https://raw.githubusercontent.com/twamix/sing-box-installer/main"
 
 proxy_uuid=""
 proxy_name=""
